d002b2876d13d59a79f3db00deda584ea787397b
samuel-yepes <syepesmolina@gmail.com>
2026-02-24 16:07:05 -0500

    Ajustes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Zap } from "lucide-react";
+import logo from "@/assets/logo.png";
 
 const Footer = () => {
   return (
@@ -6,7 +7,7 @@ const Footer = () => {
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
         <div className="flex items-center gap-2">
           <img
-            src="/src/assets/logo.png"
+            src={logo}
             alt="Logo"
             className="h-12 md:h-14 lg:h-16 w-auto object-contain transition-all duration-300"
           />
